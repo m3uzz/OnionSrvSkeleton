0bf52e5cdf35d5b9c1fb1ce0b893cbebdbfc8abd
Humberto S Lourenco <betto@m3uzz.com>
2016-04-07 03:55:13 -0300

    Base

---
+++ client/onionapp.com/service/OnionApp/config/help.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	"OnionApp" => array(
+		"OnionApp" => array(
+			"onionApp" => array(
+				"desc" => "Action description",
+				"params" => array(
+					"paramName" => "Param value"
+				),
+			),
+		),
+	),
+);
